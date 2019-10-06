53159454c74a7c85d4d54c192b3e4f8a4acf5fba
kyle felter <felterkyle94@gmail.com>
2019-10-06 14:02:44 -0400

    more

--- testing/test.sh
+++ testing/test.sh
@@ -1,4 +1,9 @@
-echo "GET http://localhost:8080/status" | vegeta attack -duration=5s | tee results.bin | vegeta report
-vegeta report -type=json results.bin > metrics.json
-cat results.bin | vegeta plot > plot.html
-cat results.bin | vegeta report -type="hist[0,100ms,200ms,300ms]"
+DIR=results$(date +%s)
+mkdir $DIR
+(
+    cd $DIR
+    cat ../targets.txt | vegeta attack -rate=100 -duration=$1s | tee results.bin | vegeta report
+    vegeta report -type=json results.bin > metrics.json
+    cat results.bin | vegeta plot > plot.html
+    cat results.bin | vegeta report -type="hist[0,100ms,200ms,300ms]"
+)
