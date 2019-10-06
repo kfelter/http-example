500b0d26348f8728171e5ffb0e3f6335a1595175
kyle felter <felterkyle94@gmail.com>
2019-10-06 14:03:41 -0400

    more

---
+++ cmd/wiki/handlers.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
