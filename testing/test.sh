7b967f5811ba82d42c61dbc2a54d63547c30c75e
kyle felter <felterkyle94@gmail.com>
2019-10-22 21:48:51 -0400

    test

--- testing/test.sh
+++ testing/test.sh
@@ -2,7 +2,7 @@ DIR=results$(date +%s)
 mkdir $DIR
 (
     cd $DIR
-    cat ../targets.txt | vegeta attack -rate=100 -duration=$1s | tee results.bin | vegeta report
+    cat ../targets.txt | vegeta attack -rate=100 -duration=20s | tee results.bin | vegeta report
     vegeta report -type=json results.bin > metrics.json
     cat results.bin | vegeta plot > plot.html
     cat results.bin | vegeta report -type="hist[0,100ms,200ms,300ms]"
